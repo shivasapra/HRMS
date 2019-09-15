5baa1cb88419051b8e20e3c807f78c6d3dc23146
shivasapra <you@example.com>
2019-09-15 12:16:33 +0530

    Attendance Setup

---
+++ app/Attendance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $table = 'attendances';
+
+    public function user(){
+        return $this->belongsTo('App\user');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
         return $this->hasMany('App\Feedbacks');
     }
 
+    public function Attendance(){
+        return $this->belongsTo('App\Attendance');
+    }
+
 }
